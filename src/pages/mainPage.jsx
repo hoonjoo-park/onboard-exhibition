f50df58203e3da33edd67d43f290f84ade71d5fe
hoonjoo94 <hoonjoo94@naver.com>
2022-01-30 04:59:40 +0900

    🎉 init

---
+++ src/pages/mainPage.jsx
@@ -0,0 +1,10 @@
+import { Carousel } from 'components/Carousel/Carousel';
+import React from 'react';
+
+export const MainPage = () => {
+  return (
+    <div>
+      <Carousel />
+    </div>
+  );
+};
