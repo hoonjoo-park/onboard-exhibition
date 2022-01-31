d2a4b2107e7fdecb63f8c67b6c95b709ccaf9687
hoonjoo94 <hoonjoo94@naver.com>
2022-01-31 16:17:05 +0900

    ✨ 슬라이더의 무한 슬라이딩 기능 구현 완료

--- src/components/Carousel/Card.jsx
+++ src/components/Carousel/Card.jsx
@@ -1,10 +1,31 @@
 import { CARD_STYLE } from 'constants';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
-export const Card = ({ title }) => {
+export const Card = ({ title, cardIndex, currentIndex, transition }) => {
+  const [myClass, setMyClass] = useState('');
+  useEffect(() => {
+    const getClassName = () => {
+      if (cardIndex === currentIndex) {
+        setMyClass('center');
+      } else if (
+        cardIndex === currentIndex - 1 ||
+        cardIndex === currentIndex + 1
+      ) {
+        setMyClass('around');
+      } else if (
+        cardIndex === currentIndex - 2 ||
+        cardIndex === currentIndex + 2
+      ) {
+        setMyClass('back');
+      } else {
+        setMyClass('');
+      }
+    };
+    getClassName();
+  }, [currentIndex, cardIndex]);
   return (
-    <CardBox>
+    <CardBox className={myClass} transition={transition}>
       <h1>{title}</h1>
     </CardBox>
   );
@@ -22,7 +43,21 @@ const CardBox = styled.li`
   border-radius: ${CARD_STYLE.radius};
   background-color: dodgerblue;
   opacity: 0.7;
+  z-index: 10;
+  transition: ${(props) => props.transition};
   h1 {
     font-size: 2rem;
+  }
+  &.center {
+    transform: scale(1);
+    z-index: 10;
+  }
+  &.around {
+    transform: scale(0.7);
+    z-index: 7;
+  }
+  &.back {
+    transform: scale(0.4);
+    z-index: 3;
   }
 `;
