b4262777c3edfdf7ff578a2f7b8256ffa7add7fd
hoonjoo94 <hoonjoo94@naver.com>
2022-01-31 17:49:47 +0900

    💄 카드들이 한 박스에 5개씩 차곡히 겹쳐보이도록 UI 수정

--- src/components/Carousel/Card.jsx
+++ src/components/Carousel/Card.jsx
@@ -8,16 +8,14 @@ export const Card = ({ title, cardIndex, currentIndex, transition }) => {
     const getClassName = () => {
       if (cardIndex === currentIndex) {
         setMyClass('center');
-      } else if (
-        cardIndex === currentIndex - 1 ||
-        cardIndex === currentIndex + 1
-      ) {
-        setMyClass('around');
-      } else if (
-        cardIndex === currentIndex - 2 ||
-        cardIndex === currentIndex + 2
-      ) {
-        setMyClass('back');
+      } else if (cardIndex === currentIndex - 1) {
+        setMyClass('around left');
+      } else if (cardIndex === currentIndex + 1) {
+        setMyClass('around right');
+      } else if (cardIndex === currentIndex - 2) {
+        setMyClass('back left');
+      } else if (cardIndex === currentIndex + 2) {
+        setMyClass('back right');
       } else {
         setMyClass('');
       }
@@ -42,7 +40,7 @@ const CardBox = styled.li`
   border: 2px solid tomato;
   border-radius: ${CARD_STYLE.radius};
   background-color: dodgerblue;
-  opacity: 0.7;
+  /* opacity: 0.7; */
   z-index: 10;
   transition: ${(props) => props.transition};
   h1 {
@@ -53,11 +51,21 @@ const CardBox = styled.li`
     z-index: 10;
   }
   &.around {
-    transform: scale(0.7);
     z-index: 7;
   }
+  &.around.left {
+    transform: scale(0.8) translateX(${CARD_STYLE.width / 1.5}rem);
+  }
+  &.around.right {
+    transform: scale(0.8) translateX(-${CARD_STYLE.width / 1.5}rem);
+  }
   &.back {
-    transform: scale(0.4);
     z-index: 3;
+  }
+  &.back.left {
+    transform: scale(0.7) translateX(${CARD_STYLE.width * 1.5}rem);
+  }
+  &.back.right {
+    transform: scale(0.7) translateX(-${CARD_STYLE.width * 1.5}rem);
   }
 `;
